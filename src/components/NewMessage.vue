765040347c3cdd6d65537cc986a71ad27269a2b6
notFromHere <jakubwasiljew@gmail.com>
2018-12-02 23:04:31 +0100

    passing param to routes

---
+++ src/components/NewMessage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="new-message">
+        <form @submit.prevent="addMessage">
+            <label for="new-message">New Message (enter to add):</label>
+            <input type="text" name="new-message" v-model="newMessage">
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'NewMessage',
+        props: [ 'name' ],
+        data() {
+            return {
+                newMessage: null
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
